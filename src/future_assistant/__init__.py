b201774f7cb29f4c018e081a10394d84e4021caa
Zaid Hijazi <z3x1337@gmail.com>
2026-07-20 16:45:13 +0300

    Expose verified runtime and skill primitives

--- src/future_assistant/__init__.py
+++ src/future_assistant/__init__.py
@@ -3,19 +3,36 @@
 from .config import AssistantConfig
 from .domain import Action, ActionKind, Plan, RuntimeResult, RuntimeStatus, VolumeOperation
 from .runtime import AssistantRuntime, DryRunEffects, SystemEffects, build_runtime
+from .verified_runtime import VerifiedAssistantRuntime, build_verified_runtime
+from .verified_skills import (
+    ConfirmationPolicy,
+    ExecutionReceipt,
+    HashChainedReceiptLedger,
+    SkillManifest,
+    SkillRisk,
+    VerifiedSkillEngine,
+)
 
 __all__ = [
     "Action",
     "ActionKind",
     "AssistantConfig",
     "AssistantRuntime",
+    "ConfirmationPolicy",
     "DryRunEffects",
+    "ExecutionReceipt",
+    "HashChainedReceiptLedger",
     "Plan",
     "RuntimeResult",
     "RuntimeStatus",
+    "SkillManifest",
+    "SkillRisk",
     "SystemEffects",
+    "VerifiedAssistantRuntime",
+    "VerifiedSkillEngine",
     "VolumeOperation",
     "build_runtime",
+    "build_verified_runtime",
 ]
 
 __version__ = "1.0.0"
